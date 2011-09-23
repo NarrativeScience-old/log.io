87409e5bb8322a0ca1adb27ad12a33c0da43fdad
Mike Smathers <msmathers@narrativescience.com>
2011-09-23 17:40:11 -0500

    Readme, configure, package.json updates, adds version history

--- bin/configure.js
+++ bin/configure.js
@@ -4,23 +4,26 @@ try {
   e("Checking for node.js package 'socket.io'...");
   var io = require('socket.io');
   var version = io.version;
-  if (version.indexOf("0.7") >= 0) {
-    e("ERROR: Log.io is currently incompatible with socket.io v0.7");
-    e("Be sure to use v0.6.17");
-    e("");
-    e("sudo npm install -g socket.io@0.6.17");
-    e("... or ...");
-    e("https://github.com/LearnBoost/socket.io/tree/0.6.17");
+  if (version.indexOf("0.8") >= 0) {
+    e("ERROR: Log.io is incompatible with pre-0.8.x versions of socket.io");
     process.exit(1);
   }
 } catch (err) {
   e("ERROR: Could not find socket.io package");
-  e("sudo npm install -g socket.io@0.6.17");
+  e("sudo npm install -g socket.io");
   e("... or ...");
-  e("https://github.com/LearnBoost/socket.io/tree/0.6.17");
-  e("");
-  e("WARNING: Log.io is currently incompatible with socket.io v0.7");
-  e("Be sure to use v0.6.17");
+  e("https://github.com/LearnBoost/socket.io/");
+  process.exit(1);
+}
+
+try {
+  e("Checking for node.js package 'socket.io-client'...");
+  var io = require('socket.io-client');
+} catch (err) {
+  e("ERROR: Could not find socket.io-client package");
+  e("sudo npm install -g socket.io-client");
+  e("... or ...");
+  e("https://github.com/LearnBoost/socket.io-client/");
   process.exit(1);
 }
 
@@ -43,28 +46,6 @@ try {
   e("sudo npm install -g connect");
   e("... or ...");
   e("https://github.com/senchalabs/connect");
-  process.exit(1);
-}
-
-try {
-  e("Checking for node.js package 'Socket.io-node-client (msmathers)'...");
-  var client_io = require('Socket.io-node-client/io-client.js').io;
-} catch (err) {
-  e("ERROR: Could not find Socket.io-node-client (msmathers) package");
-  e("https://github.com/msmathers/Socket.io-node-client");
-  e("");
-  e("NOTE: This one is tricky since there's no npm package, as well as");
-  e("a git tag checkout to downgrade its socket.io dependency.");
-  e("This is a temporary workaround until socket.io-client v0.7 works");
-  e("outside a browser context.");
-  e("");
-  e("# Install:");
-  e("cd /usr/local/lib/node_modules/");
-  e("sudo git clone git://github.com/msmathers/Socket.io-node-client.git");
-  e("cd Socket.io-node-client");
-  e("sudo git submodule update --init --recursive");
-  e("cd socket.io-node");
-  e("sudo git checkout 0.6.17");
   process.exit(1);
 }
 
