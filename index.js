75e95cf394154bf8dc22a261160a4d3a29c38849
Mike Smathers <msmathers@narrativescience.com>
2013-02-04 17:40:05 -0600

    Removed old codebase

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  Harvester: require('./lib/harvester/log_harvester.js').LogHarvester,
-  Server: require('./lib/server/log_server.js').LogServer,
-  WebClient: require('./lib/client/js/web_client.js').WebClient,
-  http: require('./lib/server/http_server.js')
-}
