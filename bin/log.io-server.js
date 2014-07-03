ba06620d7fc8f9db25e865187d3f311671b08aad
s-ko <aleks@s-ko.net>
2014-07-03 09:25:47 +0100

    Server bin tidy up

--- bin/log.io-server.js
+++ bin/log.io-server.js
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
-winston = require('winston');
-logging = new winston.Logger({
-  transports: [ new winston.transports.Console({
-    level: 'error'
-  })]
-});
-homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-webConf = require(homeDir + '/.log.io/web_server.conf').config;
-webConf.logging = logging;
-logConf = require(homeDir + '/.log.io/log_server.conf').config;
-logConf.logging = logging;
-server = require('../index.js');
-logServer = new server.LogServer(logConf);
-webServer = new server.WebServer(logServer, webConf);
-webServer.run();
+/*global require: true */
+
+(function () {
+	'use strict';
+
+	var winston = require('winston'),
+		logging = new winston.Logger({
+			transports: [
+				new winston.transports.Console({
+					level: 'error',
+				}),
+			],
+		}),
+		homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
+		webConf = require(homeDir + '/.log.io/web_server.conf').config,
+		logConf = require(homeDir + '/.log.io/log_server.conf').config,
+		server = require('../index.js'),
+		logServer,
+		webServer;
+	
+	webConf.logging = logging;
+	logConf.logging = logging;
+	logServer = new server.LogServer(logConf);
+	webServer = new server.WebServer(logServer, webConf);
+	webServer.run();
+}());
