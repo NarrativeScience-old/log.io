bf74f04a944a9bcffd3c8ec950b3dd1e83d0d4bc
s-ko <aleks@s-ko.net>
2014-07-03 11:20:05 +0100

    Variable tidy up

--- bin/log.io-harvester.js
+++ bin/log.io-harvester.js
@@ -5,18 +5,19 @@
 	'use strict';
 
 	var winston = require('winston'),
-		homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
-		conf = require(homeDir + '/.log.io/harvester.conf').config,
+		logger = new winston.Logger({
+			transports: [
+				new winston.transports.Console({
+					level: 'debug',
+				}),
+			],
+		}),
+		home_dir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
+		conf = require(home_dir + '/.log.io/harvester.conf').config,
 		Harvester = require('../index.js'),
 		harvester;
 
-	conf.logging = new winston.Logger({
-		transports: [
-			new winston.transports.Console({
-				level: 'debug'
-			}),
-		]
-	});
+	conf.logging = logger;
 	harvester = new Harvester.LogHarvester(conf);
 	harvester.run();
 }());
--- bin/log.io-server.js
+++ bin/log.io-server.js
@@ -5,22 +5,22 @@
 	'use strict';
 
 	var winston = require('winston'),
-		logging = new winston.Logger({
+		logger = new winston.Logger({
 			transports: [
 				new winston.transports.Console({
 					level: 'error',
 				}),
 			],
 		}),
-		homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
-		webConf = require(homeDir + '/.log.io/web_server.conf').config,
-		logConf = require(homeDir + '/.log.io/log_server.conf').config,
+		home_dir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
+		webConf = require(home_dir + '/.log.io/web_server.conf').config,
+		logConf = require(home_dir + '/.log.io/log_server.conf').config,
 		server = require('../index.js'),
 		logServer,
 		webServer;
 	
-	webConf.logging = logging;
-	logConf.logging = logging;
+	webConf.logging = logger;
+	logConf.logging = logger;
 	logServer = new server.LogServer(logConf);
 	webServer = new server.WebServer(logServer, webConf);
 	webServer.run();
