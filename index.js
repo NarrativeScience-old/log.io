a004ce658e1db69be6cbe0718b601278ccea3c3c
Mike Smathers <msmathers@narrativescience.com>
2020-01-10 08:46:59 -0600

    Remove outdated code

--- index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  LogHarvester: require('./lib/harvester.js').LogHarvester,
-  LogServer: require('./lib/server.js').LogServer,
-  WebServer: require('./lib/server.js').WebServer
-}
